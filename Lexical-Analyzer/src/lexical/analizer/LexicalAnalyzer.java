dba86ee9f386125ad51c8e3090bc730bf3b3be74
DiegoCortezUMG <98113773+DiegoCortezUMG@users.noreply.github.com>
2023-04-30 14:35:38 -0600

    analizador léxico, versión 1

--- Lexical-Analyzer/src/lexical/analizer/LexicalAnalyzer.java
+++ Lexical-Analyzer/src/lexical/analizer/LexicalAnalyzer.java
@@ -1,38 +1,33 @@
-
 package lexical.analizer;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-
 /**
- *
- * @author Thiago Luiz Nunes
+ * Analizador léxico Jaguar
+ * Proyecto compiladores, fase 2
  */
 public class LexicalAnalyzer {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException, IOException {
-
-    // Open the file
+    // Abre el archivo
     FileInputStream fstream = new FileInputStream("src/lexical/analizer/textfile");
     BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
 
     String strLine;
     ArrayList<String> lines = new ArrayList<String> ();
 
-    //Read File Line By Line
+    // Lee línea por línea
     while ((strLine = br.readLine()) != null)   {
       lines.add(strLine);
         System.out.println(strLine);
     }
-    //Close the input stream
+    // Cierra el flujo de tokens
     br.close();
     
     Tokens token = new Tokens(lines);
--- Lexical-Analyzer/src/lexical/analizer/Tokens.java
+++ Lexical-Analyzer/src/lexical/analizer/Tokens.java
@@ -1,72 +1,67 @@
-
 package lexical.analizer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
 /**
- *
- * @author Thiago Luiz Nunes
+ * Definición de tokens y patrones
  */
 public class Tokens {
-    
-    protected ArrayList<String> keyWords = new ArrayList<>(Arrays.asList("program", "var", "integer", "real", "boolean", "procedure", "begin",
-                    "end", "if", "then", "else", "while", "do", "not"));
-    
-    protected ArrayList<String> delimiters = new ArrayList<>(Arrays.asList(";",".",":","(",")",","));
-    protected ArrayList<String> operatiorsR = new ArrayList<>(Arrays.asList("=","<",">","<=",">=","<>"));
-    protected ArrayList<String> operatiorsA = new ArrayList<>(Arrays.asList("+","-","or"));
-    protected ArrayList<String> operatiorsM = new ArrayList<>(Arrays.asList("*","/","and"));
-    protected ArrayList<String> booleanDigits = new ArrayList<>(Arrays.asList("true", "false"));
-    
-    
-    protected String attributtion = ":=";
-    protected String identifiers = "([a-z]|[A-Z])([0-9]|[a-z]+|[A-Z]|_)*";
-    protected String intDigits = "[0-9]+";
-    protected String floatDigits = "[0-9]+[.][0-9]+";
-    
-    protected ArrayList<String> myProgram = null;
+    public static final String reset = "\u001B[0m";
+    public static final String rojo = "\u001B[31m";
+
+    ArrayList<String> palabrasReservadas = new ArrayList<>(Arrays.asList("sellado","abstracto","clase", "imprimir", "ent", "escribir", "booleano", "const", "raiz",
+                    "flota","doble","bit", "si", "then", "demas", "durante", "hacer", "mientras", "publico", "privado", "interno", "protegido", "vacia","vacio", "larg","cad","."));
+    //aquí el punto se utiliza para marcar el final de una línea de código
+    ArrayList<String> delimitadores = new ArrayList<>(Arrays.asList(";",".","",",","{","}"));
+    ArrayList<String> comparadores = new ArrayList<>(Arrays.asList("=:=","!=",">","<",">=","<=","++","&&"));
+    ArrayList<String> operadoresM = new ArrayList<>(Arrays.asList("+","-","*","/"));
+    ArrayList<String> simbolo = new ArrayList<>(Arrays.asList("#","%","$"));
+    ArrayList<String> parentesis = new ArrayList<>(Arrays.asList("(",")"));
+    ArrayList<String> booleanos = new ArrayList<>(Arrays.asList("verdadero", "falso"));
+
+    String asignacion = "=";
+    String identificador = "[@]+([a-z]|[A-Z])([0-9]|[a-z]+|[A-Z]|_)*";
+    String textoString = "[\"]([a-z]|[A-Z])([0-9]|[a-z]+|[A-Z]|_)*[\"]";
+    String DigitosInt = "[0-9]+";
+    String DigitosFloat = "[0-9]+[.][0-9]+";
+
+    ArrayList<String> myProgram = null;
 
     public Tokens(ArrayList<String> myArrayList) {
         this.myProgram = myArrayList;
     }
-    
     public int lexicAnalizer() {
-        
         for(int i = 0; i < this.myProgram.size(); i++) {
             String line = discardComments(this.myProgram.get(i));
             String[] splited = line.split("\\s+");
-            
-            //Verify line by line of the program
+            // revisa el programa línea por línea
             for (int j = 0; j < splited.length; j++) {
                 String token = splited[j];
                 String result = checkToken(token);
-                if(!result.equals("unknown")) {
-//                    System.out.println(result + " : " + token);
+                if(!result.equals("desconocido (1)")) {
                     System.out.println(token + " | "+ result + " |" + i);
                 }
                 else {
                     String subToken = token.substring(0, token.length() - 1);
                     result = checkToken(subToken);
-                    if(!result.equals("unknown")) {
-//                        System.out.println(result + " : " +subToken);
-                        System.out.println(subToken + " | "+ result + " |" + i);
-                        
+                    if(!result.equals("desconocido (2)")) {
+                        System.out.println(result + " : " +subToken);
+                        //System.out.println(subToken + " | "+ result + " |" + i);
                         String lastToken = token.substring(token.length() - 1, token.length());
                         result = checkToken(lastToken);
-                        if(!result.equals("unknown")) {
-//                            System.out.println(result + " : " +lastToken);
-                            System.out.println(lastToken + " | "+ result + " |" + i);
+                        if(!result.equals("desconocido (3)")) {
+                            System.out.println(result + " : " +lastToken);
+                            //System.out.println(lastToken+ " | "+ result + " |" + i);
                         }
                         else {
-                            System.err.println("Error: Does not belong to language!");
-                            System.err.println("Line: " + i + ", Status: " + result + " -> " + subToken);
+                            System.err.println("Error: No pertenece al lenguage (1)");
+                            //System.err.println("Linea: "+i+", Estado: "+result+" -> "+subToken);
                             return 0;
                         }
                     }
                     else {
-                        System.err.println("Error: Does not belong to language!");
-                        System.err.println("Line: " + i + ", Status: " + result + " -> " + subToken);
+                        System.err.println("Error: No pertenece al languaje (2)");
+                        //System.err.println("Linea: "+i+", Estado: " + result + " -> "+subToken+"");
                         return 0;
                     }
                 }
@@ -74,35 +69,39 @@ public class Tokens {
         }
         return 1;
     }
-    
     private String discardComments(String line) {
         return line.replaceAll("\\{(?s).*?}", "");
     }
-    
     private String checkToken(String split) {
-
-        if (this.keyWords.contains(split)) {
-            return "Keyword";
-        } else if (this.delimiters.contains(split)) {
-            return "Delimiter";
-        } else if (this.operatiorsR.contains((split))) {
-            return "Comparison Operator";
-        } else if (this.operatiorsA.contains(split)) {
-            return "Additive Operator";
-        } else if (this.operatiorsM.contains(split)) {
-            return "Multiplicative Operator";
-        } else if (split.matches(this.intDigits)){
-            return "Integer Digit";
-        } else if (split.matches(this.floatDigits)) {
-            return "Floating Point Digit";
-        } else if (split.matches(this.identifiers)) {
-            if (this.booleanDigits.contains(split)) {
-                return "Boolean Digit";
+        if (this.palabrasReservadas.contains(split)) {
+            return "Palabra reservada";
+        } else if (split.matches(this.textoString)) {
+            return "cadena de texto";
+        }else if (this.parentesis.contains(split)) {
+            return "parentesis";
+        } else if (this.asignacion.contains(split)) {
+            return "asignación";
+        } else if (this.delimitadores.contains(split)) {
+            return "Delimitador";
+        } else if (this.comparadores.contains((split))) {
+            return "Comparador";
+        } else if (this.operadoresM.contains(split)) {
+            return "Operación arimética";
+        } else if (this.simbolo.contains(split)) {
+            return "caracter especial";
+        } else if (split.matches(this.DigitosInt)){
+            return "Entero";
+        } else if (split.matches(this.DigitosFloat)) {
+            return "Decimal";
+        } else if (split.matches(this.identificador)) {
+            if (this.booleanos.contains(split)) {
+                return "Booleano";
             }
-            return "Identifier";
+            return "Identificador";
         } else if (split.matches("\\{(?s).*?")) {
-            return "unknown";
+            return "no pertenece al lenguaje/n";
+        } else{
+        return rojo+"error: token desconocido"+reset;
         }
-        return "unknown";
-    }    
+    }
 }
